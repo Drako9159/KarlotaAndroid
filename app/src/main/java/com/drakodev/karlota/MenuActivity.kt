857a834d721fe4324c366cd75ddfee58fc6002a0
drako9159 <chochito9159@gmail.com>
2023-06-01 12:10:18 -0600

    configure assets

---
+++ app/src/main/java/com/drakodev/karlota/MenuActivity.kt
@@ -0,0 +1,31 @@
+package com.drakodev.karlota
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.drakodev.karlota.firstapp.FirstAppActivity
+import com.drakodev.karlota.imccalculator.ImcCalculatorActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener { navigateToSaludApp() }
+        btnIMCApp.setOnClickListener { navigateToImcApp() }
+
+    }
+
+    private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludApp(){
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/drakodev/karlota/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,15 @@
+package com.drakodev.karlota.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drakodev.karlota.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+
+
+
+    }
+}
