a69fc123d0ee94ad0a1c64527563e2b4a8a83a90
drako9159 <chochito9159@gmail.com>
2023-06-03 11:39:09 -0600

    add new app superheroapp

---
+++ app/src/main/java/com/drakodev/karlota/superheroapp/SuperHeroListActivity.kt
@@ -0,0 +1,14 @@
+package com.drakodev.karlota.superheroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drakodev.karlota.R
+
+class SuperHeroListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_super_hero_list)
+    }
+
+}
